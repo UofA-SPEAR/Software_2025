376025f4c648b90a022f6674bc93d544eaa448eb
AydenBravender <aydenbravender@gmail.com>
2025-02-22 14:35:14 -0700

    enabled GNSS fusion

--- rover/src/kipp_state/launch/kipp_state.launch.py
+++ rover/src/kipp_state/launch/kipp_state.launch.py
@@ -22,8 +22,9 @@ def generate_launch_description():
         executable='joint_state_publisher',
         name='joint_state_publisher',
         output='screen',
-        parameters=[{'robot_description': robot_description, 'source_list':["wheel_encoders/"]}]
+        parameters=[{'robot_description': robot_description, 'source_list':["wheel_encoders"]}]
     )
+
     # Define the robot_state_publisher node
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
