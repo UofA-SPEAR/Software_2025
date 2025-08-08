58af1e035e5d02188908d60dcab46ef8dd0c6ae5
Indy1103 <83981742+Indy1103@users.noreply.github.com>
2025-08-08 03:12:44 -0600

    Create night_camera_1_base.sh

---
+++ night_camera_1_base.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PORT=5040
+gst-launch-1.0 udpsrc port=$PORT caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" \
+    ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink
