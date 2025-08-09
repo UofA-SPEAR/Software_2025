f5338acd372250f3b712807140ab86b0fbe3baff
Induwara <kandpah@ualberta.ca>
2025-08-09 03:30:14 -0600

    pov cam scripts

--- rover/startup_scripts/base_boneyard.sh
+++ rover/startup_scripts/base_boneyard.sh
@@ -2,4 +2,6 @@
 
 # gnome-terminal -- bash -c "export ROS_DOMAIN_ID=10; source /home/prjadmin/Desktop/Software_2025/install/setup.bash; ros2 launch kipp joy_launch.py"
 
-gnome-terminal -- bash -c "/home/spear1/Desktop/Software_2025/rover/camera_launch/base_camera.sh"
+gnome-terminal -- bash -c "/home/spear1/Desktop/Software_2025/rover/camera_launch/base_arm_cam.sh"
+
+gnome-terminal -- bash -c "/home/spear1/Desktop/Software_2025/rover/camera_launch/base_pov_cam.sh"
