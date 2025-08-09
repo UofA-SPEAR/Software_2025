4e5a17ea85f2857f3ddcf9cfcf102d3fe717ca30
Induwara <kandpah@ualberta.ca>
2025-08-09 03:17:57 -0600

    spear1

--- rover/startup_scripts/base_boneyard.sh
+++ rover/startup_scripts/base_boneyard.sh
@@ -2,4 +2,4 @@
 
 # gnome-terminal -- bash -c "export ROS_DOMAIN_ID=10; source /home/prjadmin/Desktop/Software_2025/install/setup.bash; ros2 launch kipp joy_launch.py"
 
-gnome-terminal -- bash -c "/home/prjadmin/Desktop/Software_2025/rover/camera_launch/base_camera.sh"
+gnome-terminal -- bash -c "/home/spear1/Desktop/Software_2025/rover/camera_launch/base_camera.sh"
--- rover/startup_scripts/rover_boneyard.sh
+++ rover/startup_scripts/rover_boneyard.sh
@@ -2,4 +2,4 @@
 
 # gnome-terminal -- bash -c "export ROS_DOMAIN_ID=10; source /home/prjadmin/Desktop/Software_2025/install/setup.bash; ros2 launch kipp roverlaunch.py"
 
-gnome-terminal -- bash -c "/home/prjadmin/Desktop/Software_2025/rover/camera_launch/rover_camera.sh"
+gnome-terminal -- bash -c "/home/spear1/Desktop/Software_2025/rover/camera_launch/rover_camera.sh"
